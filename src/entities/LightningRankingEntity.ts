c97bfb4c10cb63ce526ef1237c81498b6ba3736c
Diango Gavidia <diango.code@gmail.com>
2025-11-21 19:18:16 -0400

    chore: linter

--- src/entities/LightningRankingEntity.ts
+++ src/entities/LightningRankingEntity.ts
@@ -1,12 +1,12 @@
 import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
-    Index
+	Column,
+	CreateDateColumn,
+	Entity,
+	Index,
+	JoinColumn,
+	ManyToOne,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
 } from "typeorm";
 
 import { UserProfileEntity } from "./UserProfileEntity";
@@ -14,31 +14,31 @@ import { UserProfileEntity } from "./UserProfileEntity";
 @Entity({ name: "lightning_rankings" })
 @Index(["userId", "season"], { unique: true })
 export class LightningRankingEntity {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
 
-    @Column({ name: "user_id" })
-    userId: string;
+	@Column({ name: "user_id" })
+	userId: string;
 
-    @ManyToOne(() => UserProfileEntity)
-    @JoinColumn({ name: "user_id" })
-    user: UserProfileEntity;
+	@ManyToOne(() => UserProfileEntity)
+	@JoinColumn({ name: "user_id" })
+	user: UserProfileEntity;
 
-    @Column({ default: 0 })
-    points: number;
+	@Column({ default: 0 })
+	points: number;
 
-    @Column({ name: "tournaments_won", default: 0 })
-    tournamentsWon: number;
+	@Column({ name: "tournaments_won", default: 0 })
+	tournamentsWon: number;
 
-    @Column({ name: "tournaments_played", default: 0 })
-    tournamentsPlayed: number;
+	@Column({ name: "tournaments_played", default: 0 })
+	tournamentsPlayed: number;
 
-    @Column()
-    season: string;
+	@Column()
+	season: string;
 
-    @CreateDateColumn({ name: "created_at" })
-    createdAt: Date;
+	@CreateDateColumn({ name: "created_at" })
+	createdAt: Date;
 
-    @UpdateDateColumn({ name: "updated_at" })
-    updatedAt: Date;
+	@UpdateDateColumn({ name: "updated_at" })
+	updatedAt: Date;
 }
